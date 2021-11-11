021c67abd559003438fdae7419206516f2afe116
ZachWang <815810395@qq.com>
2021-11-11 21:02:48 +0800

    evaluate R2

--- fit_vProf_paras.m
+++ fit_vProf_paras.m
@@ -4,12 +4,19 @@ function [a, R2]=fit_vProf_paras(x,y)
 %velocity to the maximum velocity
 
 a0=[0.6; 2.2];     %initial guess
-[a,r]=nlinfit(x,y,@uFunc,a0);
+[a]=nlinfit(x,y,@uFunc,a0);
 
 %plot and compare
-eta_vec=linspace(0,1,100).'
+eta_vec=linspace(0,1,100).';
 u2u_p=u_withdrawLayer(a(1), a(2), eta_vec);
 plot(y, x, 'bo', u2u_p, eta_vec, 'r'); 
+
+%calculate coefficient of determination
+ym=mean(y);
+u2u_at_x=u_withdrawLayer(a(1), a(2), x);
+SS_res=sum((y-u2u_at_x).^2);
+SS_tot=sum((y-ym).^2);
+R2=1-SS_res/SS_tot;
 
     function uPredic=uFunc(a, x)
         uPredic=u_withdrawLayer(a(1), a(2), x);
